5ce2b5e9230f9985b8ae712620f97a9f026280d5
andrew dalton and kimlan nguyen <andrew+kimlan@>
2017-06-13 08:33:05 -0700

    be our initial commit upon project creation.

---
+++ Objects/Students.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace UniversityRegistrar
+{
+  public class Students
+  {
+    
+  }
+}
---
+++ Tests/StudentsTest.cs
@@ -0,0 +1,28 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace UniversityRegistrar
+{
+  [Collection("UniversityRegistrar")]
+  public class StudentsTest : IDisposable
+  {
+    public StudentsTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb; Initial Catalog=university_test; Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Test_DatabaseEmptyAtFirst()
+    {
+     //Arrange, Act
+     int result = Students.GetAll().Count;
+
+     //Assert
+     Assert.Equal(0, result);
+    }
+
+  }
+}
